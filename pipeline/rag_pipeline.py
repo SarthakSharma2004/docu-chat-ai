e2578d94ff8439cc68f14ce0d250c9b4c3359022
Sarthak <2sarthaksharma@gmail.com>
2025-11-29 17:32:15 +0530

    added memory to rag pipeline

--- pipeline/rag_pipeline.py
+++ pipeline/rag_pipeline.py
@@ -1,19 +1,18 @@
+from parser.document_loader import DocumentLoader
 from rag.retriever import Retriever
 from rag.vector_store import VectorStore
-from langchain_core.documents import Document
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from prompts.rag_prompt import RagPrompt
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_core.language_models import BaseChatModel
-from parser.document_loader import DocumentLoader
 from rag.splitter import DocumentSplitter
-from prompts.contextualize_prompt import ContextualizePrompt    
 from prompts.rag_prompt import RagPrompt
+from prompts.contextualize_prompt import ContextualizePrompt  
+from memory.redis_memory import get_session_history
+
+from langchain_core.language_models import BaseChatModel
+from langchain_core.documents import Document
+
 from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from memory.redis_memory import get_session_history
+
 
 
 
@@ -84,13 +83,13 @@ class RagPipeline:
         
 
     # ---- RAG + MEMORY CHAIN (REDIS) ---------
-    def build_rag_with_memory(self, session_id: str):
+    def build_rag_with_memory(self):
         if not self.rag_chain:
             self.build_rag_chain()
         
         conversational_rag_chain = RunnableWithMessageHistory(
             self.rag_chain,
-            lambda: get_session_history(session_id),
+            get_session_history,
             input_messages_key= "input",
             history_messages_key= "chat_history",
             output_messages_key= "answer"
@@ -99,13 +98,22 @@ class RagPipeline:
 
         return self.conversational_rag_chain
     
-
     
-    def user_input(self, session_id: str, user_input: str):
+    # ---- queries with history ------------
+    
+    def user_input(self, session_id: str, question: str):
         if not self.conversational_rag_chain:
-            self.build_rag_with_memory(session_id)
+            self.build_rag_with_memory()
+
+        try:
+            response = self.conversational_rag_chain.invoke(
+                {"input": question},
+                config= {"configurable": {"session_id": session_id}}
+            )
+            return response['answer']
         
-        return self.conversational_rag_chain.invoke({"input": user_input})
+        except Exception as e:
+            raise RuntimeError(f"Error processing user input: {e}")
     
       
 
