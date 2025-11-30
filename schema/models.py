5b32314f309e3fc52eba44703a02b45cdae42d65
Sarthak <2sarthaksharma@gmail.com>
2025-12-01 02:39:10 +0530

    added pydantic response and request models

--- schema/models.py
+++ schema/models.py
@@ -4,5 +4,12 @@ class QueryRequest(BaseModel):
     session_id: str = Field(..., description="Session ID, Unique session identifier.")
     question: str = Field(..., description="User's question.")
 
+
+class Source(BaseModel):
+    content: str = Field(description= "Content of the source document.")
+    meta: str = Field(default="Unknown", description= "Metadata (Source document) of the source document.")
+    page: str = Field(default="N/A", description= "Page number of the source document.")
+
 class QueryResponse(BaseModel):
     answer: str = Field(..., description="Answer to the user's question.")
+    sources: list[Source] = Field(default= [], description= "Sources of the answer.")
